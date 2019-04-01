602413fa5f006eb53297784c22a81aa2e703b623
wild <240440056@qq.com>
2019-04-01 11:10:05 +0800

    deployment sh

--- deployment.sh
+++ deployment.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 App='deployment'
-ProjectHome='/home/wild/deployment
+ProjectHome='/home/wild/deployment'
 RunHome='/opt/deployment'
 Time=`date '+%Y%m%d%H%M%S'`
 
 cd $ProjectHome
 git pull
 
-kill -9 $(ps -ef|grep java|awk '/'$App.js'/{print $2}')
+kill -9 $(ps -ef|grep node|awk '/'$App.js'/{print $2}')
 
 cp -f $RunHome/$App.js $RunHome/$App-$Time.js
 cp -f $ProjectHome/$App.js $RunHome/$App.js
